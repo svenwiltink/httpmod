107f9ef90a41343872040097ac7161c72b7e2ff8
Sven Wiltink <swiltink@transip.nl>
2020-10-05 19:06:38 +0200

    aded custom type

--- cmd/demo.go
+++ cmd/demo.go
@@ -1,20 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"httpmod"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"strings"
 )
 
 func main() {
-
 	httpmod.Apply()
 
-	req, err := http.NewRequest(http.MethodGet, "http://localhost:9090/banaan", nil)
+	req, err := http.NewRequest(http.MethodPost, "https://postman-echo.com/post", strings.NewReader(url.Values{"HEY": []string{"YOU","SUBSCRIBE"}}.Encode()))
 	if err != nil {
 		panic(err)
 	}
 
-	req.Header.Set("Custom-Header", "MyValue\nOtherKey: OtherValue")
+	oh := new(httpmod.OrderedHeader)
+	oh.Add("WOW", "so cool!")
+	oh.Add("Absolutely", "Amazing!")
+	oh.Add("User-Agent", "Bananentaart")
+	oh.Apply(req.Header)
 
-	http.DefaultClient.Do(req)
+	r, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	rb, err := httputil.DumpResponse(r, true)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(rb))
 }
--- httpmod.go
+++ httpmod.go
@@ -5,7 +5,6 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptrace"
-	"net/textproto"
 	"reflect"
 	"sort"
 	"sync"
@@ -40,6 +39,18 @@ func Remove() {
 	}
 }
 
+type OrderedHeader []string
+
+func (oh *OrderedHeader) Add(key, value string) {
+	*oh = append(*oh, key + ": " + value)
+}
+
+func (oh *OrderedHeader) Apply(header http.Header) {
+	// set to empty string to stdlib doesn't output one
+	header["User-Agent"] = []string{}
+	header["Custom-Headers"] = *oh
+}
+
 //go:linkname customHeaderValidation vendor/golang.org/x/net/http/httpguts.ValidHeaderFieldValue
 func customHeaderValidation(a string) bool
 
@@ -51,68 +62,20 @@ func cursedWriteSubset(h http.Header, w io.Writer, exclude map[string]bool, trac
 	if !ok {
 		ws = stringWriter{w}
 	}
-	kvs, sorter := sortedKeyValues(h, exclude)
-	var formattedVals []string
-	for _, kv := range kvs {
-		for _, v := range kv.values {
-			v = textproto.TrimString(v)
-			for _, s := range []string{kv.key, ": ", v, "\r\n"} {
-				if _, err := ws.WriteString(s); err != nil {
-					headerSorterPool.Put(sorter)
-					return err
-				}
+
+	customHeader, exists := h["Custom-Headers"]
+	if !exists {
+		return nil
+	}
+
+	for _, v := range customHeader {
+		for _, s := range []string{v, "\r\n"} {
+			if _, err := ws.WriteString(s); err != nil {
+				return err
 			}
-			if trace != nil && trace.WroteHeaderField != nil {
-				formattedVals = append(formattedVals, v)
-			}
-		}
-		if trace != nil && trace.WroteHeaderField != nil {
-			trace.WroteHeaderField(kv.key, formattedVals)
-			formattedVals = nil
 		}
 	}
-	headerSorterPool.Put(sorter)
 	return nil
-}
-
-
-var headerSorterPool = sync.Pool{
-	New: func() interface{} { return new(headerSorter) },
-}
-
-// sortedKeyValues returns h's keys sorted in the returned kvs
-// slice. The headerSorter used to sort is also returned, for possible
-// return to headerSorterCache.
-func sortedKeyValues(h http.Header, exclude map[string]bool) (kvs []keyValues, hs *headerSorter) {
-	hs = headerSorterPool.Get().(*headerSorter)
-	if cap(hs.kvs) < len(h) {
-		hs.kvs = make([]keyValues, 0, len(h))
-	}
-	kvs = hs.kvs[:0]
-	for k, vv := range h {
-		if !exclude[k] {
-			kvs = append(kvs, keyValues{k, vv})
-		}
-	}
-	hs.kvs = kvs
-	sort.Sort(hs)
-	return kvs, hs
-}
-
-// A headerSorter implements sort.Interface by sorting a []keyValues
-// by key. It's used as a pointer, so it can fit in a sort.Interface
-// interface value without allocation.
-type headerSorter struct {
-	kvs []keyValues
-}
-
-func (s *headerSorter) Len() int           { return len(s.kvs) }
-func (s *headerSorter) Swap(i, j int)      { s.kvs[i], s.kvs[j] = s.kvs[j], s.kvs[i] }
-func (s *headerSorter) Less(i, j int) bool { return s.kvs[i].key < s.kvs[j].key }
-
-type keyValues struct {
-	key    string
-	values []string
 }
 
 // stringWriter implements WriteString on a Writer.
